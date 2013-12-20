08c3dcd65690f974bc14830503937e66cf913199
GeneChatham <genechatham@gmail.com>
2013-12-20 14:39:36 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weightable::Application.config.secret_key_base = 'aa78d7cc0030ce396a77b51264607acab77cba230bb0c506a3645998b92727506a2af3245276f76c2759876f77b2b915101fd4145434c81304525149d96b3559'
