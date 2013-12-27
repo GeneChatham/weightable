36e512859dd3f4fed142975db3f650f2425bef87
LesleyMdCampo <lesliz32@gmail.com>
2013-12-27 14:28:52 -0600

    Add Users index and show pages to check weighins. Add basic view of weighins on user show page.:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,41 @@
 class UsersController < ApplicationController
-  def index
-  end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
-  end
-
-  def edit
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to root_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :profilepic, :startweight, :desiredweight, :bio)
   end
 end
--- app/controllers/weigh_ins_controller.rb
+++ app/controllers/weigh_ins_controller.rb
@@ -1,22 +1,41 @@
 class WeighInsController < ApplicationController
-  def index
-  end
 
   def show
+    @weigh_in = Weigh_in.find(params[:id])
+  end
+
+  def index
+    @weigh_ins = Weigh_in.all
   end
 
   def new
-  end
-
-  def edit
+    @weigh_in = Weigh_in.new
   end
 
   def create
+    @weigh_in = Weigh_in.new(weigh_in_params)
+    @weigh_in.save
+    redirect_to root_path
+  end
+
+  def edit
+    @weigh_in = Weigh_in.find(params[:id])
   end
 
   def update
+    @weigh_in = Weigh_in.find(params[:id])
+    @weigh_in.update(weigh_in_params)
+    redirect_to root_path
   end
 
   def destroy
+    @weigh_in = Weigh_in.find(params[:id])
+    @weigh_in.destroy
+    redirect_to root_path
+  end
+  
+  private
+  def weigh_in_params
+    params.require(:weigh_in).permit(:user_id, :weightpic, :description, :currentweight)
   end
 end
